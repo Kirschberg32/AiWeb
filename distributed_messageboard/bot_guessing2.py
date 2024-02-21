a6c4197fc538231687b98851d2feb7d5a4e551a1
egrund <82370915+egrund@users.noreply.github.com>
2024-02-21 17:34:22 +0100

    help + icon change
    Improved bot messages in guessing bot 2 and added a help message.

--- distributed_messageboard/bot_guessing2.py
+++ distributed_messageboard/bot_guessing2.py
@@ -3,6 +3,7 @@ point guessing game bot
 """
 
 """
+Ideas: Spiel bleibt gleich bei channel neustart, Testen was bei mehreren Spielern passiert. Guesses are analysed and automatically processed. 
 
 x y Hyperplane
 
@@ -43,15 +44,15 @@ class GuessingBot2:
             if len(digit) == 1:
                 self.max_value = int(digit[0])
                 self.start()
-                return f"The maximum value was changed. \nYou can guess a point in the square ( - {self.max_value},- {self.max_value})  to ({self.max_value},{self.max_value}) now."
+                return f"The maximum value was changed. <br>You can guess a point in the square ( - {self.max_value},- {self.max_value})  to ({self.max_value},{self.max_value}) now."
             return f"Please give exactly one possible value for max_value."
+        elif self.point == []:
+            return "No active game. Type <b>'start'</b> to start a game."
         elif "reveal" in input:
             correct = self.point
             self.point = []
             self.hints = []
-            return f"The correct point is {correct}. Send 'start' to start a new game. "
-        elif self.point == []:
-            return "No active game. Send 'start' to start a game."
+            return f"The correct point is {correct}. Type <b>'start'</b> to start a new game. "
 
         digit = [i for i in input.split() if i.lstrip('-+').isdigit()]
         string = [i for i in input.split() if not i.lstrip('-+').isdigit()]
@@ -83,25 +84,25 @@ class GuessingBot2:
         # check if in right quadrant
         if hint == "quadrant":
             if (np.sign([x,y]) == np.sign(self.point)).all():
-                return "In correct quadrant"
-            return "Not in right quadrant."
+                return "Your entered point is located in the same quadrant as the point to guess."
+            return "Your entered point is located in a different quadrant than the point to guess."
 
         # check if on the same line or in what relation to line
         if hint == "hyperplane":
             steigung = y/x
-            if steigung == self.point[1]/self.point[0] or steigung == (- self.point[1]/self.point[0]):
-                return "The point is on the same line through the origin as your guess."
+            if self.point[1] == steigung * self.point[0]:
+                return "The point to guess is located on the line through the origin and your entered point."
             # check which half
-            if self.point[1] > steigung * self.point[0]:
-                return "The point is above the hyperplane. "
-            return "The point is below the hyperplane"
+            elif self.point[1] > steigung * self.point[0]:
+                return "The point to guess is located above the hyperplane in the y dimension."
+            return "The point to guess is located below the hyperplane in the y dimension."
 
         # check how far from input point
         if hint == "distance":
             dist = np.linalg.norm(np.array([x,y])-np.array(self.point))
             if dist < self.max_value/3:
-                return f"The point is less than {self.max_value/3} away from {(x,y)}."
-            return f"The point is more than {self.max_value/3} away from {(x,y)}."
+                return f"The point to guess is less than {self.max_value/3} away from {(x,y)}."
+            return f"The point to guess is more than {self.max_value/3} away from {(x,y)}."
         
         return "Something went wrong."
         
@@ -111,14 +112,14 @@ class GuessingBot2:
         """
         self.point = [ random.randint(- self.max_value,self.max_value), random.randint(-self.max_value,self.max_value) ]
         self.hints = ["hyperplane", "distance", "quadrant"]
-        return f"You can get 3 hints before you have to make your final guess. They are 'hyperplane', 'distance' and 'quadrant'. For a sample input type 'help'. Each hint is based on a point given by you. When you did you final guess type 'reveal' to get the correct point."
+        return f"You can get 3 hints before you have to make your final guess. They are <b>'hyperplane'</b>, <b>'distance'</b> and <b>'quadrant'</b>. For more information and a sample input type <b>'help'</b>. Each hint is based on a point entered by you. When you gave your final guess type <b>'reveal'</b> to get the correct point."
     
     def start_message(self):
         """
         Returns a message with the most important rules and how to start. 
         """
         return_string = f"This is a point guessing game. You can guess a point in the square ( - {self.max_value},- {self.max_value})  to ({self.max_value},{self.max_value})."
-        return_string += f"\nYou can change the maximum value for x and y by typing 'max_value = <n>'. \nx and y are whole numbers. \nThe game starts when you type 'start'."
+        return_string += f"<br>You can change the maximum value for x and y by typing <b>'max_value n'</b>. Replace n with a number. The x and y coordinates of the point to guess are whole numbers. <br>The game starts when you type <b>'start'</b>."
         return return_string
     
     def is_start(self,message : str):
@@ -130,5 +131,27 @@ class GuessingBot2:
             return True 
         return False
     
-    def help(self):
-        return ""
+    def help(self): 
+        """
+        Information about:
+
+        - Goal of the game
+        - How to start the game.
+        - Which hints, what do they do & how to use them. 
+        - How to guess.
+        - How to reveal the correct point. 
+        """
+
+        return_string = f""" This is a point guessing game. The goal is to guess a 2D point in the square ( - {self.max_value},- {self.max_value})  to ({self.max_value},{self.max_value}), or rather to be as close as possible to it. The x and y coordinates of the point to guess are whole numbers.
+        <br>{self.max_value} is the maximum value and the difficulty of the game depends on it. It can be changed by typing <b>'max_value n'</b>. Replace n with a number.
+        <br>To start the game type <b>'start'</b>. You can also use this command to restart the game at any time. Be aware that the correct point will be changed. 
+
+        <h2> Hints </h2>
+        To be able to make a better guess, you can get 3 hints before making your final guess and 'reveal' the solution. They are 'hyperplane', 'distance' and 'quadrant'. You can use each of them exactly one time. To ask for a hint simply type the name of the hint with the coordinates of a point in the same message separated by a space, as each hint depends on a point (Example: <b>'1 1 hyperplane'</b>).
+        <br><b>'hyperplane':</b> Imagine a straight line through your entered point and the origin (0,0). This is the hyperplane. Your hint will be whether the searched point is located on this line, or in the y dimension above or below the line. 
+        <br><b>'distance':</b> You will get information about whether the point to guess is less or more than max_value / 3 away from your entered point. 
+        <br><b>'quadrant':</b> You will get the information if your entered point is located in the same quadrant of the coordinate system as the point to guess. If it is, this means, that both coordinates of the point to guess have the same signs as the ones from your entered point. 
+        
+        <br><br>After getting all the hints you want, you can make a final guess. If you want you can send it in the chat simply by sending two numbers separated by a space (<b>'3 3'</b>). If you are done guessing you can reveal the correct point by typing <b>'reveal'</b>. The closest guess wins. 
+        """
+        return return_string
